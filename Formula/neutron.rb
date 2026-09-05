4fb58383d57b490ed7592bcff32060a72d719380
PandaBytez <pandabytez@pm.me>
2026-09-05 17:13:06 +0200

    chore(formula): bump neutron to v0.1.0

--- Formula/neutron.rb
+++ Formula/neutron.rb
@@ -2,7 +2,7 @@ class Neutron < Formula
   desc "Fast WireGuard profile manager via NetworkManager"
   homepage "https://github.com/PandaBytez/neutron"
   url "https://github.com/PandaBytez/neutron/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "fc5eb96d38a678988337c97b74511e3a58c5377d36dcd13230192de0f7cca118"
+  sha256 "ae031bf60c082dbc7197036fad707001622899692bca561600b0e6e933939c79"
   license "GPL-3.0-or-later"
 
   depends_on "rust" => :build
